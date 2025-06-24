8b479b0be3555510a36fc6e584c4b4af01696bd4
Jeisson22 <jeisson.15.ga@gmail.com>
2025-06-24 15:25:56 -0500

    Analisis y Diseño del Sistema

---
+++ src/test/java/PMM/Retail/Product/Management/Module/ProductManagementModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package PMM.Retail.Product.Management.Module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
